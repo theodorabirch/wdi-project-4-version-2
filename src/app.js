a6b9b5c0648a55b400ff6d6006d94ea372169eaf
David Comer <davidjcomer@aol.com>
2018-12-08 14:38:47 +0000

    rendered Home Page. Fixed Babel File. Bulma Added

--- src/app.js
+++ src/app.js
@@ -4,24 +4,29 @@ import 'bulma';
 import './scss/style.scss';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Header from '.components/Header';
+import Header from './components/Header';
 import Home from './components/Home';
+import UserShow from './components/users/Show';
+import MealIndex from './components/meals/Index';
+import MealShow from './components/meals/Show';
 
 class App extends React.Component {
 
   render() {
     return(
-      <div>
-        <Header />
-        <main>
-          <Switch>
-            <Route exact path ='/' component={Home}/>
-            <Route exact path ='/dashboard' component={Dashboard}/>
-            <Route path ='/user/:id/meals' component={MealIndex}/>
-            <Route path ='/user/:id/meal/:id' component={MealShow}/>
-          </Switch>
-        </main>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <main>
+            <Switch>
+              <Route exact path ='/' component={Home}/>
+              <Route exact path ='/dashboard' component={UserShow}/>
+              <Route path ='/user/:id/meals' component={MealIndex}/>
+              <Route path ='/user/:id/meal/:id' component={MealShow}/>
+            </Switch>
+          </main>
+        </div>
+      </BrowserRouter>
     );
   }
 }
