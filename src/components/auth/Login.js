eb92137af7df4ccac254bdcdf8635af0c0d269be
theodorabirch <theodorabirch@gmail.com>
2018-12-10 10:44:03 +0000

    merge

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { saveToken } from '../../lib/auth';
 import { handleChange } from '../../lib/common';
-// import { createFlashMessage } from '../../lib/flash';
+
 
 class AuthLogin extends React.Component {
   constructor(props) {
@@ -20,10 +20,7 @@ class AuthLogin extends React.Component {
         saveToken(res.data.token);
       })
       .then(() => this.props.history.push(`/users/${this.props.match.params.id}`));
-    // .catch((error) => {
-    //   createFlashMessage(error.response.data.message, 'danger');
-    //   this.props.history.replace('/login');
-    // });
+
   }
 
   render() {
