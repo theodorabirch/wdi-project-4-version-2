dfbc3a8fea196f4303a3ab75bc941a276ebc038a
theodorabirch <theodorabirch@gmail.com>
2018-12-10 14:48:56 +0000

    user thumbnail completed

--- src/components/users/UserThumbnail.js
+++ src/components/users/UserThumbnail.js
@@ -5,18 +5,34 @@ import React from 'react';
 function UserThumbnail({ user }) {
   return(
     <div>
-      <img src="{user.profilePicture }"/>
-      <div>
-        <span> { user.forename } {user.surname }</span>
-        <span> { user.username }</span>
-        <span> { user.age }</span>
-        <i className="fas fa-{user.sex}"></i>
+      <div className="stat-box">
+        <img src="{user.profilePicture }" alt="Sample Image" className="item-image" />
       </div>
-      <div>
-        <div>Height: { user.height }cm</div>
-        <div>Weight: { user.weight }kg</div>
-        <div>BMI: { user.BMI } </div>
-        <div></div>
+
+      <div className="stat-box">
+        <div><span>{ user.forename }</span> <span>{user.surname }</span> <i className="fas fa-{user.sex}"></i></div>
+        <div>{ user.username }</div>
+        <div>{ user.age }</div>
+      </div>
+
+      <div className="stat-box">
+        <h3>Height</h3>
+        <div>{ user.height }cm</div>
+      </div>
+
+      <div className="stat-box">
+        <h3>Weight</h3>
+        <div>{ user.weight }kg</div>
+      </div>
+
+      <div className="stat-box">
+        <h3>BMI</h3>
+        <div>{ user.BMI }</div>
+      </div>
+
+      <div className="stat-box">
+        <h3>BodyFat</h3>
+        <div>user.bodyFat</div>
       </div>
     </div>
   );
